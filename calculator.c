dff33b1f8435b9387da5886e2fe89f0502e6f9eb
Kiavash Seraj <serajkx@dukes.jmu.edu>
2023-04-30 19:55:14 -0400

    finished calc?

--- calculator.c
+++ calculator.c
@@ -15,45 +15,121 @@ Submitted on: 05/01/2023
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include "message.h"
 /* -------------------------------------------------------------------*/
+
+#define MAXBUFLEN 256
+#define IPSTRLEN 50
+typedef struct sockaddr SA ;
+/*------------------------------------------------------------------------
+* Error Handling Functions
+*----------------------------------------------------------------------*/
+void err_sys(const char* msg)
+{
+    fflush( stderr ) ;
+    perror( msg ) ;
+    exit( 1 );
+}
+
+
 int main(int argc, char *argv[])
 {
 
-    int sockfd = socket(AF_INET, SOCK_DGRAM, 0); // SOCK_DGRAM for UDP
 
-    int req_write_fd = atoi(argv[0]);
-    int rep_read_fd = atoi(argv[1]);
-    printf("This is the Calculator process (id = %d).\t\n", getpid());
-    printf("Calculator waiting to receive from FD %d\n", req_write_fd);
 
     int num1;
     char op;
     int num2;
-
-    read(req_write_fd, &num1, sizeof(num1));
-    read(req_write_fd, &op, sizeof(op));
-    read(req_write_fd, &num2, sizeof(num2));
-
-    printf("\tCalculator received the following: %d %c %d from FD %d\n", num1, op, num2, req_write_fd);
-
     int result;
-    switch (op)
-    {
-    case '+':
-        result = num1 + num2;
-        break;
-    case '-':
-        result = num1 - num2;
-        break;
-    case '*':
-        result = num1 * num2;
-        break;
-    case '/':
-        result = num1 / num2;
-        break;
-    default:
-        printf("\nINVALID operation\n");
-        result = -9999;
-    };
+    
 
-    printf("Calculator sent the following to the User: %d on FD %d\n", result, rep_read_fd);
+    struct sockaddr_in serverSocket, clientSocket;
+    unsigned short port = 13;
+    time_t now;
+    char buf[ MAXBUFLEN ];
+    char ipAddress[ IPSTRLEN ];
+    char timeStr[MAXBUFLEN ];
+
+    calcMsg received_message;
+    ssize_t received_len
+
+    unsigned int addressLength;
+
+
+    int socketfd = socket(AF_INET, SOCK_DGRAM, 0); // SOCK_DGRAM for UDP
+    if (socketfd < 0) {
+        err_sys("Could NOT create socket\n");
+    }
+
+    // Prepare the server's socket address structure
+    memset((void *)&serverSocket, 0, sizeof(serverSocket));
+    serverSocket.sin_family = AF_INET;
+    serverSocket.sin_addr.s_addr = htonl(INADDR_ANY);
+    serverSocket.sin_port = htons(port);
+
+    printf("\nThis is the Calculator server developed by Kiavash Seraj & Mateen Faieq\n");
+    
+    if ( bind(socketfd, (struct sockaddr *)&serverSocket, sizeof(serverSocket)) < 0 ) {
+        snprintf( buf, MAXBUFLEN, "Could NOT bind to port %d\n", port);
+        err_sys(buf);
+    }
+
+    inet_ntop (AF_INET, (void *)&serverSocket.sin_addr.s_addr, ipAddress, IPSTRLEN);
+    printf("\nBound socket %d to IP %s Port %d\n", sd, ipAddress, ntohs(serverSocket.sin_port));
+    
+
+    while(1) {
+        addressLength = sizeof(clientSocket);
+        printf("\nCALCULATOR server waiting for Requests\n");
+
+        if (recvfrom(socketfd, &received_message, sizeof(calcMsg), 0, (struct sockaddr *) &clientSocket, &addressLength) < 0){
+            err_sys("recvfrom");
+        }
+
+        received_message.msgType = ntohl(received_message.msgType);
+        received_message.num1 = ntohl(received_message.num1);
+        received_message.num2 = ntohl(received_message.num2);
+        received_message.operation = ntohl(received_message.operation);
+        received_message.result = ntohl(received_message.result);
+        received_message.fullName = ntohl(received_message.fullName);
+
+        num1 = received_message.num1;
+        num2 = received_message.num2;
+        op = received_message.operation;
+
+        
+        printf("CALCULATOR server recieved: { Name='%s' Type=%d , %d %c %d , result=\t%d }\n");
+        inet_ntop(AF_INET, (void *) &clientSocket.sin_addr.s_addr, ipAddress, IPSTRLEN);
+        printf("\tfrom IP %s port %d\n", ipAddress, ntohs(clientSocket.sin_port));
+
+        switch (op)
+        {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            result = num1 / num2;
+            break;
+        default:
+            printf("\nINVALID operation\n");
+            result = -9999;
+        };
+
+        calcMsg send_message;
+        send_message.msgType = 2;
+        send_message.num1 = num1;
+        send_message.num2 = num2;
+        send_message.operation = op;
+        send_message.result = result;
+        send_message.fullName = "Kiavash Seraj & Mateen Faieq-Calculator";
+
+        sendto(socketfd, (void *) &send_message, sizeof(struct calcMsg), 0, (struct sockaddr *)&clientSocket, addressLength);
+
+    }
+}
--- user.c
+++ user.c
@@ -15,6 +15,7 @@ Submitted on: 05/01/2023
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include "message.h"
 /* -------------------------------------------------------------------*/
 int main(int argc, char *argv[])
 {
@@ -24,27 +25,93 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-    int sockfd = socket(AF_INET, SOCK_DGRAM, 0); // SOCK_DGRAM for UDP
-
     char *ip_address = argv[1];
     int port = atoi(argv[2]);
     int num1 = atoi(argv[3]);
     char op = argv[4][0];
     int num2 = atoi(argv[5]);
 
-    printf("This client is developed by Mateen Faieq & Kiavash Seraj\n");
+    // Allocate a socket
+    int sockfd = socket(AF_INET, SOCK_DGRAM, 0); // SOCK_DGRAM for UDP
+    if (sockfd < 0)
+    {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Set up the server address
+    struct sockaddr_in srvrSkt;
+    memset((void *)&srvrSkt, 0, sizeof(srvrSkt));
+    srvrSkt.sin_family = AF_INET;                    // IPv4 address
+    srvrSkt.sin_addr.s_addr = inet_addr(ip_address); // Server IP address (network byte order
+    srvrSkt.sin_port = htons(atoi(argv[2]));         // Server port
+
+    calcMsg message;
+    char fullName[125];
+
+    // read myName.txt
+    FILE *fp = fopen("myName.txt", "r");
+    fgets(fullName, 125, fp);
+    fclose(fp);
+
+    // Remove newline character if present
+    size_t len = strlen(fullName);
+    if (len > 0 && fullName[len - 1] == '\n')
+    {
+        fullName[len - 1] = '\0';
+    }
+    strncpy(message.fullName, fullName, sizeof(message.fullName) - 1);
+
+    // assign message type
+    message.msgType = 1;
+    // assign operands
+    message.num1 = htonl(num1);
+    message.num2 = htonl(num2);
+    // assign operation
+    message.operation = op;
+    // set result to 0
+    message.result = 0;
+
+    printf("This client is developed by %s\n", message.fullName);
 
     printf("Attempting Calculator server at '%s' : %d\n", ip_address, port);
 
-    printf("User Mateen Faieq & Kiavash Seraj sent this message to the Calculator : { Name= }");
+    // Send the message to the server
+    socklen_t srvrSktLen = sizeof(srvrSkt);
+    if (sendto(sockfd, (void *)&message, sizeof(message), 0, (struct sockaddr *)&srvrSkt, srvrSktLen) < 0)
+    {
+        perror("sendto failed");
+        exit(EXIT_FAILURE);
+    }
 
-    
-    
+    printf("User %s sent this message to the Calculator : ", message.fullName);
+    char userMsg[130];
+    char calcMsg[130];
 
+    snprintf(userMsg, sizeof(userMsg), "%s-User", fullName);
+    strncpy(message.fullName, userMsg, sizeof(message.fullName) - 1);
+    printMsg(stdout, &message);
+    strncpy(message.fullName, fullName, sizeof(message.fullName) - 1);
+    printf("\n");
+
+    printf("User is now waiting to receive result ...\n");
+
+    // Receive the result from the server
+
+    calcMsg received_message;
+    if (recvfrom(sockfd, (void *)&received_message, sizeof(received_message), 0, (struct sockaddr *)&srvrSkt, &srvrSktLen) < 0)
+    {
+        perror("recvfrom");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("User %s received this message from the Calculator server: ", received_message.fullName);
+    snprintf(calcMsg, sizeof(calcMsg), "%s-Calc", fullName);
+    strncpy(received_message.fullName, calcMsg, sizeof(received_message.fullName) - 1);
+    printMsg(stdout, &received_message);
+    strncpy(received_message.fullName, fullName, sizeof(received_message.fullName) - 1);
+    printf("\n");
     int result;
 
-    
-
     // printf("User received the following: %d from FD %d\n", result, rep_read_fd);
-
 }
